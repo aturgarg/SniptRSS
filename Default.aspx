1139a7f7cfd85bb7380e89dec60e1be316006b8f
unknown <compaq corrner@.(none)>
2010-10-10 23:32:55 +0530

    first commit on git ( local)

---
+++ Default.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" ValidateRequest="false"
+    CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+   <div>
+       <asp:TextBox ID="txtUrl" runat="server" /> 
+    <asp:Button Text="Fetch" ID="btnFetchRssFeed" OnClick="btnFetchRssFeed_OnClick" runat="server" />
+     </div>
+    
+    <br />
+    <asp:ListView ID="ListView1" runat="server" OnItemCommand="ListView1_OnItemCommand" DataKeyNames="Url,Description"
+        onselectedindexchanged="ListView1_SelectedIndexChanged" OnSelectedIndexChanging="ListView1_OnSelectedIndexChanging"  >    
+        <LayoutTemplate>        
+            <table class="gridview">
+                <th>Select</th>
+                <th>Published</th>
+                <th>Article</th>
+                
+                <th>Tags</th>                
+                <tbody>
+                    <asp:PlaceHolder ID="itemPlaceholder" runat="server" />
+                </tbody>            
+            </table>        
+        </LayoutTemplate>        
+        <ItemTemplate>
+        
+            <tr>
+            <td>
+            <asp:Button CommandName="Select" runat="server" Text="*"  />
+            </td>
+                <td><%#Eval("Published", "{0:d}") %></td>
+                <td>
+                    <a href='<%#Eval("Url")%>'><%#Eval("Title") %></a>
+                </td>
+                
+                <td>
+                  <asp:Repeater ID="Repeater1" DataSource='<%#Eval("Tags") %>' runat="server">
+                        <ItemTemplate> <%#Container.DataItem %> </ItemTemplate>
+                        <SeparatorTemplate>,</SeparatorTemplate>
+                    </asp:Repeater>
+
+                   
+                </td>
+            </tr>        
+        </ItemTemplate>        
+    </asp:ListView>
+
+
+    <textarea id="txtDescription" cols="20" rows="2" runat="server"></textarea>
+    </asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    //static IList<object> mPostList = new List<object>();    
+  
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnFetchRssFeed_OnClick(object sender, EventArgs e)
+    {
+        try
+        {
+            XDocument xDocFeeds = XDocument.Load(txtUrl.Text.Trim());
+            xDocFeeds = XDocument.Parse(xDocFeeds.ToString().Replace("snipt:tags>", "tags>"),LoadOptions.PreserveWhitespace);
+            this.ReadXML(xDocFeeds);
+        }
+        catch (Exception ex)
+        {
+            //txtFeeds.Text = ex.Message;
+        }
+    }
+
+    private void ReadXML(XDocument xFeeds)
+    {
+        XNamespace sniptsTags = "http://snipt.net/api/tags.xml";
+
+        var posts = from item in xFeeds.Descendants("item")
+                    select new
+                    {
+                        Title = item.Element("title").Value,
+                        Published = DateTime.Parse(item.Element("pubDate").Value),
+                        Url = item.Element("link").Value,
+                        //Tags = item.Element(sniptsTags+ "tags").Value,
+                        Tags = item.Element("tags").Value.Split(' ').ToArray(),
+                        Guid = item.Element("guid").Value,
+                        Description = item.Element("description").Value
+                    };
+
+        //mPostList.Clear();
+        foreach (var item in posts)
+        {
+            //mPostList.Add((object)item);
+        }
+
+        //mPostList = (List<object>)posts;
+        
+        ListView1.DataSource = posts;
+        //ListView1.DataSource = mPostList;
+        ListView1.DataBind();
+
+    }
+
+    protected void ListView1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        //var item = ListView1.SelectedValue;
+        
+        //
+    }
+
+    protected void ListView1_OnSelectedIndexChanging(object sender, ListViewSelectEventArgs  e)
+    {
+        if(e.NewSelectedIndex > -1)
+        {
+            txtDescription.InnerHtml = ListView1.DataKeys[e.NewSelectedIndex].Values["Description"].ToString();
+        }
+    }
+
+    protected void ListView1_OnItemCommand(object source, ListViewCommandEventArgs e)
+    {
+
+        if (e.CommandName == "Select")
+        {
+            //int rowIndex = ((ListViewDataItem)e.Item).DisplayIndex;
+            //object selectedObj = mPostList[rowIndex];            
+        }
+    }
+}
+
+/*
+ <asp:Repeater ID="Repeater1" DataSource='<%#Eval("Tags") %>' runat="server">
+                        <ItemTemplate> <%#Container.DataItem %> </ItemTemplate>
+                        <SeparatorTemplate>,</SeparatorTemplate>
+                    </asp:Repeater>
+ * 
+ * <%#Eval("Tags") %>
+ * 
+ * http://snipt.net/aturgarg/feed
+ * 
+ * http://snipt.net/api/users/aturgarg.xml
+ * 
+ * http://snipt.net/api/snipts/14983.xml?style=autumn
+ * 
+ * 
+*/
